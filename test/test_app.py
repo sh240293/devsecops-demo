f86dc092cf4031d1a8a60936c44d790e0208120b
Shanish <shanish7best@gmail.com>
2025-11-15 10:05:24 +0530

    gitleaks ci added

--- test/test_app.py
+++ test/test_app.py
@@ -3,4 +3,4 @@ from app import app
 def test_home():
     response = app.test_client().get('/')
     assert response.status_code == 200
-    assert b"Hello, Secure!" in response.data
+    assert b"Hello, Secure World!" in response.data
